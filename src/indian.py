e8d5a2a4048eee2a4e840cb13d1cb760ff72aae5
Joshvince911 <149005674+Joshvince911@users.noreply.github.com>
2026-01-15 02:51:41 +0000

    This is the autoncode, basically it simply goes to the forward set of balls with the intake running collects them, this is what I have for nwo, but i will add the loading in part later but right now it says no module named vex

---
+++ src/indian.py
@@ -0,0 +1,20 @@
+
+from vex import *
+
+# Initialize les motors
+left_drive = Motor(Ports.PORT1)
+right_drive = Motor(Ports.PORT2)
+intake_motor = Motor(Ports.PORT3)
+
+def autonomous ():
+    intake_motor.spin (FORWARD, 160, RPM) # type: ignore
+    left_drive.spin (FORWARD, 160, RPM)
+    right_drive.spin(FORWARD, 160, RPM)
+
+    wait(1.2, SECONDS)
+
+    left_drive.stop()
+    right_drive.stop()
+    intake_motor.stop()
+
+
