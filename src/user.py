f3c87545fd97b7cc72ff471c93a78a6408106954
Arnav-Yadav529 <566782@my.cuhsd.org>
2026-02-02 18:25:28 -0800

    Update user.py
    Refactor user control logic to use drivetrain for movement and update intake motor controls.

--- src/user.py
+++ src/user.py
@@ -1,10 +1,3 @@
-from vex import *
-
-# Brain
-brain = Brain()
-
-# Motor Definition
-
 # Right Motors
 motor_1b = Motor(Ports.PORT4)
 motor_2b = Motor(Ports.PORT5)
@@ -19,13 +12,12 @@ motor_3a = Motor(Ports.PORT3)
 motor_group_1 = MotorGroup(motor_1a, motor_2a, motor_3a)
 motor_group_2 = MotorGroup(motor_1b, motor_2b, motor_3b)
 
+# Drivetrain
+drivetrain = DriveTrain(motor_group_1,motor_group_2)
+
 # Intake Motors
 motor_intake_1 = Motor(Ports.PORT8)
 motor_intake_2 = Motor(Ports.PORT9)
-
-# Pneumatics
-matchloader = Pneumatic(Brain.three_wire_port.a)
-descore = Pneumatic(Brain.three_wire_port.b)
 
 # Controller
 controller_1 = Controller()
@@ -38,50 +30,26 @@ def user_control():
     # Setting up controller for user control portion
     while True:
         wait(20,MSEC)
-        brain.screen.clear_screen()
-        brain.screen.print("driver control")
-        # Setting up controller for user control portion
-        
-        # Left motor group
-        if controller_1.buttonUp.pressing():
-            motor_group_1.spin(FORWARD,60,PERCENT)
-        elif controller_1.buttonDown.pressing():
-            motor_group_1.spin(REVERSE,60,PERCENT)
-        else:
-            motor_group_1.stop()
+        # Right joystick for moving forward and backward
+        if controller_1.axis2.position()>10:
+            drivetrain.drive(FORWARD,controller_1.axis2.position(),PERCENT)
+        elif controller_1.axis2.position()<-10:
+            drivetrain.drive(REVERSE,abs(controller_1.axis2.position()),PERCENT)
 
-        # Right motor group
-        if controller_1.buttonX.pressing():
-            motor_group_2.spin(FORWARD,60,PERCENT)
-        elif controller_1.buttonB.pressing():
-            motor_group_2.spin(REVERSE,60,PERCENT)
-        else:
-            motor_group_2.stop()
+        # Left joystick for moving left and right
+        if controller_1.axis4.position()>10:
+            drivetrain.turn(RIGHT,controller_1.axis4.position(),PERCENT)
+        elif controller_1.axis4.position()<-10:
+            drivetrain.turn(LEFT,abs(controller_1.axis4.position()),PERCENT)
 
         # Intake pull
-        if controller_1.buttonR1.pressing():
-            motor_intake_1.spin(FORWARD,60,PERCENT)
-        elif controller_1.buttonR2.pressing():
-            motor_intake_1.spin(REVERSE,60,PERCENT)
-        else:
-            motor_intake_1.stop()
+        if controller_1.buttonL1.pressing():
+            motor_intake_1.spin(FORWARD,100,PERCENT)
+        elif controller_1.buttonL2.pressing():
+            motor_intake_1.spin(REVERSE,100,PERCENT)
 
         # Intake discharge
-        if controller_1.buttonL1.pressing():
-            motor_intake_2.spin(FORWARD,60,PERCENT)
-        elif controller_1.buttonL2.pressing():
-            motor_intake_2.spin(REVERSE,60,PERCENT)
-        else:
-            motor_intake_2.stop()
-
-        # Matchloader
-        if controller_1.buttonUp.pressing():
-            matchloader.open()
-        else:
-            matchloader.close()
-
-        # Descore
-        if controller_1.buttonX.pressing():
-            descore.open()
-        else:
-            descore.close()
+        if controller_1.buttonR1.pressing():
+            motor_intake_2.spin(FORWARD,100,PERCENT)
+        elif controller_1.buttonR2.pressing():
+            motor_intake_2.spin(REVERSE,100,PERCENT)
