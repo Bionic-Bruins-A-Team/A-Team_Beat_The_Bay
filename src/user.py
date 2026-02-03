dc1a7e8621f6041256eca0ba93b8b202b0f0e0d9
Arnav-Yadav529 <566782@my.cuhsd.org>
2026-02-03 15:21:36 -0800

    Update user.py
    Removed unused motor and controller initializations.

--- src/user.py
+++ src/user.py
@@ -1,36 +1,11 @@
-from vex import *
-brain = Brain()
-
-# Right Motors
-motor_1b = Motor(Ports.PORT4)
-motor_2b = Motor(Ports.PORT5)
-motor_3b = Motor(Ports.PORT6)
-
-# Left Motors
-motor_1a = Motor(Ports.PORT1)
-motor_2a = Motor(Ports.PORT2)
-motor_3a = Motor(Ports.PORT3)
-
-# Motor Groups
-motor_group_1 = MotorGroup(motor_1a, motor_2a, motor_3a)
-motor_group_2 = MotorGroup(motor_1b, motor_2b, motor_3b)
-
 # Drivetrain
 drivetrain = DriveTrain(motor_group_1,motor_group_2)
-
-# Intake Motors
-motor_intake_1 = Motor(Ports.PORT8)
-motor_intake_2 = Motor(Ports.PORT9)
-
-# Controller
-controller_1 = Controller()
 
 brain.screen.clear_screen()
 brain.screen.print("driver control")
 
 # User control function setting up controller and handling buttons
 def user_control():
-    # Setting up controller for user control portion
     while True:
         wait(20,MSEC)
         # Right joystick for moving forward and backward
