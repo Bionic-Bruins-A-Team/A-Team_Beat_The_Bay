a02945f4ef5d532b7bf74af54a349eac8d6c1110
Arnav-Yadav529 <566782@my.cuhsd.org>
2026-02-04 14:47:43 -0800

    Update user.py

--- src/user.py
+++ src/user.py
@@ -4,30 +4,40 @@ drivetrain = DriveTrain(motor_group_1,motor_group_2)
 brain.screen.clear_screen()
 brain.screen.print("driver control")
 
-# User control function setting up controller and handling buttons
+# User control function setting up controller and handling joystick/button inputs
 def user_control():
-    while True:
-        wait(20,MSEC)
-        # Right joystick for moving forward and backward
-        if controller_1.axis2.position()>10:
-            drivetrain.drive(FORWARD,controller_1.axis2.position(),PERCENT)
-        elif controller_1.axis2.position()<-10:
-            drivetrain.drive(REVERSE,abs(controller_1.axis2.position()),PERCENT)
+  while True:
+      wait(20,MSEC)
+      back_forth = controller_1.axis3.position()
+      left_right = controller_1.axis1.position()
 
-        # Left joystick for moving left and right
-        if controller_1.axis4.position()>10:
-            drivetrain.turn(RIGHT,controller_1.axis4.position(),PERCENT)
-        elif controller_1.axis4.position()<-10:
-            drivetrain.turn(LEFT,abs(controller_1.axis4.position()),PERCENT)
+      # Right joystick for moving forward and backward
+      if back_forth > 10:
+          drivetrain.drive(FORWARD,back_forth,PERCENT)
+      elif back_forth < -10:
+          drivetrain.drive(REVERSE,abs(back_forth),PERCENT)
 
-        # Intake pull
-        if controller_1.buttonL1.pressing():
-            motor_intake_1.spin(FORWARD,100,PERCENT)
-        elif controller_1.buttonL2.pressing():
-            motor_intake_1.spin(REVERSE,100,PERCENT)
+      # Left joystick for moving left and right
+      if left_right > 10:
+          drivetrain.turn(RIGHT,left_right,PERCENT)
+      elif left_right < -10:
+          drivetrain.turn(LEFT,abs(left_right),PERCENT)
 
-        # Intake discharge
-        if controller_1.buttonR1.pressing():
-            motor_intake_2.spin(FORWARD,100,PERCENT)
-        elif controller_1.buttonR2.pressing():
-            motor_intake_2.spin(REVERSE,100,PERCENT)
+      elif (back_forth > -10 and back_forth < 10) and (left_right > -10 and left_right < 10):
+           drivetrain.stop()
+
+      # Intake pull
+      if controller_1.buttonL1.pressing():
+          motor_intake_1.spin(FORWARD,100,PERCENT)
+      elif controller_1.buttonL2.pressing():
+          motor_intake_1.spin(REVERSE,100,PERCENT)
+      else:
+           motor_intake_1.stop()
+
+      # Intake discharge
+      if controller_1.buttonR1.pressing():
+          motor_intake_2.spin(FORWARD,100,PERCENT)
+      elif controller_1.buttonR2.pressing():
+          motor_intake_2.spin(REVERSE,100,PERCENT)
+      else:
+           motor_intake_2.stop()
