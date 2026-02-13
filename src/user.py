ddb85ba3f3ca8649bcb96e4ce3ab6e34b16b6570
Arnav-Yadav529 <566782@my.cuhsd.org>
2026-02-12 22:07:49 -0800

    Update user.py

--- src/user.py
+++ src/user.py
@@ -1,36 +1,32 @@
-# Drivetrain
-drivetrain = DriveTrain(motor_group_1,motor_group_2)
 from vex import *
 
 # Brain
 brain = Brain()
 
-# Devices
+# Right Motors
+motor_1b = Motor(Ports.PORT4,GearSetting.RATIO_6_1)
+motor_2b = Motor(Ports.PORT5,GearSetting.RATIO_6_1)
+motor_3b = Motor(Ports.PORT6,GearSetting.RATIO_6_1)
 
 # Left Motors
-motor_1a = Motor(Ports.PORT1)
-motor_2a = Motor(Ports.PORT2)
-motor_3a = Motor(Ports.PORT3)
-
-# Right Motors
-motor_1b = Motor(Ports.PORT4)
-motor_2b = Motor(Ports.PORT5)
-motor_3b = Motor(Ports.PORT6)
+motor_1a = Motor(Ports.PORT1,GearSetting.RATIO_6_1,True)
+motor_2a = Motor(Ports.PORT2,GearSetting.RATIO_6_1,True)
+motor_3a = Motor(Ports.PORT3,GearSetting.RATIO_6_1,True)
 
 # Motor Groups
 motor_group_1 = MotorGroup(motor_1a, motor_2a, motor_3a)
 motor_group_2 = MotorGroup(motor_1b, motor_2b, motor_3b)
 
 # Drivetrain
-drivetrain = SmartDrive(motor_group_1,motor_group_2)
+drivetrain = DriveTrain(motor_group_1,motor_group_2)
 
 # Intake Motors
 motor_intake_1 = Motor(Ports.PORT8)
 motor_intake_2 = Motor(Ports.PORT9)
 
 # Pneumatics
-pneumatic_1 = Pneumatics(brain.three_wire_port.a)
-pneumatic_2 = Pneumatics(brain.three_wire_port.b)
+descore = Pneumatics(brain.three_wire_port.a)
+matchloader = Pneumatics(brain.three_wire_port.b)
 
 # Controller
 controller_1 = Controller()
@@ -46,18 +42,22 @@ def user_control():
       left_right = controller_1.axis1.position()
 
       # Right joystick for moving forward and backward
-      if back_forth > 10:
+      if back_forth >= 10:
           drivetrain.drive(FORWARD,back_forth,PERCENT)
-      elif back_forth < -10:
+      elif back_forth <= -10:
           drivetrain.drive(REVERSE,abs(back_forth),PERCENT)
 
       # Left joystick for moving left and right
-      if left_right > 10:
+      if 10 < left_right <= 20:
+          drivetrain.turn(RIGHT,20,PERCENT)
+      elif left_right > 20:
           drivetrain.turn(RIGHT,left_right,PERCENT)
-      elif left_right < -10:
+      elif -20 <= left_right < -10:
+          drivetrain.turn(LEFT,20,PERCENT)
+      elif left_right < -20:
           drivetrain.turn(LEFT,abs(left_right),PERCENT)
 
-      elif (back_forth > -10 and back_forth < 10) and (left_right > -10 and left_right < 10):
+      elif -10 < back_forth < 10 and -10 < left_right < 10:
            drivetrain.stop()
 
       # Intake pull
@@ -75,40 +75,9 @@ def user_control():
           motor_intake_2.spin(REVERSE,100,PERCENT)
       else:
            motor_intake_2.stop()
-    # Setting up controller for user control portion
-    while True:
-        wait(20,MSEC)
-        # Right joystick for moving forward and backward
-        if controller_1.axis2.position()>10:
-            drivetrain.spin(FORWARD,controller_1.axis2.position(),PERCENT)
-        elif controller_1.axis2.position()<-10:
-            drivetrain.spin(REVERSE,abs(controller_1.axis2.position()),PERCENT)
 
-        # Left joystick for moving left and right
-        if controller_1.axis4.position()>10:
-            drivetrain.turn(RIGHT,controller_1.axis4.position(),PERCENT)
-        elif controller_1.axis4.position()<-10:
-            drivetrain.turn(LEFT,abs(controller_1.axis4.position()),PERCENT)
-
-        # Intake pull
-        if controller_1.buttonL1.pressing():
-            motor_intake_1.spin(FORWARD,100,PERCENT)
-        elif controller_1.buttonL2.pressing():
-            motor_intake_1.spin(REVERSE,100,PERCENT)
-
-        # Intake discharge
-        if controller_1.buttonR1.pressing():
-            motor_intake_2.spin(FORWARD,100,PERCENT)
-        elif controller_1.buttonR2.pressing():
-            motor_intake_2.spin(REVERSE,100,PERCENT)
-
-        # Pneumatics
-        if controller_1.buttonUp.pressing():
-            pneumatic_1.open()
-        else:
-            pneumatic_1.close()
-
-        if controller_1.buttonX.pressing():
-            pneumatic_2.open()
-        else:
-            pneumatic_2.close()
+      # Descore
+      if controller_1.buttonUp.pressed():
+          descore.open()
+      elif controller_1.buttonDown.pressed():
+          descore.close()
