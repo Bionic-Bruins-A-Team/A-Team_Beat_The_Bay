a4cffbe4287fe93c677c39ba2d7b2897274d9ef3
Arnav-Yadav529 <566782@my.cuhsd.org>
2026-02-13 17:34:43 -0800

    Refactor main.py with user control and autonomous functions
    Refactor main.py to improve structure and readability.

--- src/main.py
+++ src/main.py
@@ -9,8 +9,6 @@
 
 # Library imports
 from vex import *
-from autonomous import *
-from user import *
 
 brain = Brain()
  # Setting up motors and controller
@@ -37,9 +35,65 @@ inertial_1 = Inertial(Ports.PORT7)
 
 brain.screen.print("Hello Vex World!")
 
+def user_control():
+ while True:
+      wait(20,MSEC)
+      back_forth = controller_1.axis3.position()
+      left_right = controller_1.axis1.position()
+
+      # Right joystick for moving forward and backward
+      if back_forth >= 10:
+          drivetrain.drive(FORWARD,back_forth,PERCENT)
+      elif back_forth <= -10:
+          drivetrain.drive(REVERSE,abs(back_forth),PERCENT)
+
+      # Left joystick for moving left and right
+      if 10 < left_right <= 20:
+          drivetrain.turn(RIGHT,20,PERCENT)
+      elif left_right > 20:
+          drivetrain.turn(RIGHT,left_right,PERCENT)
+      elif -20 <= left_right < -10:
+          drivetrain.turn(LEFT,20,PERCENT)
+      elif left_right < -20:
+          drivetrain.turn(LEFT,abs(left_right),PERCENT)
+
+      elif -10 < back_forth < 10 and -10 < left_right < 10:
+           drivetrain.stop()
+
+      # Intake pull
+      if controller_1.buttonL1.pressing():
+          motor_intake_1.spin(FORWARD,100,PERCENT)
+      elif controller_1.buttonL2.pressing():
+          motor_intake_1.spin(REVERSE,100,PERCENT)
+      else:
+           motor_intake_1.stop()
+
+      # Intake discharge
+      if controller_1.buttonR1.pressing():
+          motor_intake_2.spin(FORWARD,100,PERCENT)
+      elif controller_1.buttonR2.pressing():
+          motor_intake_2.spin(REVERSE,100,PERCENT)
+      else:
+           motor_intake_2.stop()
+
+      # Descore
+      if controller_1.buttonUp.pressed():
+          descore.open()
+      elif controller_1.buttonDown.pressed():
+          descore.close()
+
+      # Matchloader
+      if controller_1.buttonX.pressed():
+          matchloader.open()
+      elif controller_1.buttonB.pressed():
+          matchloader.close()
+
+def autonomous():
+ pass
 
 # create competition instance
 comp = Competition(user_control, autonomous)
 
 # actions to do when the program starts
+
 brain.screen.clear_screen()
